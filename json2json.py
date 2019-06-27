d077bb88acf2995c3f247e52999861fbb8f32144
anavalo <tsournos.anastasios@gmail.com>
2019-06-27 08:16:08 +0200

    .

--- json2json.py
+++ json2json.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from bs4 import BeautifulSoup as bs
 import time
 
+#function for retrieving abstracts from pubmed
 def get_abstract(url):
     data = requests.get(url)
     soup = bs(data.text, 'html.parser')
@@ -15,21 +16,23 @@ with open(Path('data', 'trainining7b.json')) as jsonf:
 
 paragraphs = []
 
+#find the "factoid" indexes of training7b.json
 idx = []
 for i, j in enumerate(seven_b_data):
     if j['type'] == 'factoid':
         idx.append(i)
 
 for index in idx:
-    counter = 000
+    counter = 0
     for url in seven_b_data[index]['documents']:
-        dict = {'context': get_abstract(url), 'qas': [{'question': seven_b_data[index]['body'], 'id': (seven_b_data[index]['id']+'_'+str(counter))}]}
-        print(dict)
-        counter += 001
+        dict = {'context': get_abstract(url),
+                'qas': [{'question': seven_b_data[index]['body'],
+                         'id': (seven_b_data[index]['id']+'_'+str("%03d" % counter))}]}
+        paragraphs.append(dict)
+        counter += 1
         time.sleep(0.30)
 
-
-
+training7b
 
 
 
